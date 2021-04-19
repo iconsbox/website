f92d0e15ce187c92466485da8a78b2ff261ddca6
sayjeyhi <sayjeyhi@gmail.com>
2021-04-19 14:54:08 +0430

    fix CTA and blog post

--- src/pages/home/Integrations.tsx
+++ src/pages/home/Integrations.tsx
@@ -2,9 +2,10 @@
 import React from "react"
 import { jsx, Grid, Box, Flex } from "theme-ui"
 
-import { BrandSketchIcon, BrandReactIcon } from "@iconbox/fa5"
+import { BrandSketchIcon, BrandReactIcon, BrandGithubIcon } from "@iconbox/fa5"
 import { LogoNpmIcon, LogoAngularIcon } from "@iconbox/ion"
-import { BrandGithubIcon } from "@iconbox/fa5"
+
+import { Link } from "gatsby"
 import FigmaIcon from "./icons/Figma"
 import IconJar from "./icons/IconJar"
 import XDIcon from "./icons/XD"
@@ -12,9 +13,10 @@ import PSDIcon from "./icons/PSD"
 import IllustratorIcon from "./icons/Illustrator"
 import Svelte from "./icons/Svelte"
 import Vue from "./icons/Vue"
+import Layout from "../../components/layout"
 
-const Integrations = () => {
-  return (
+const Integrations = () => (
+  <>
     <Grid sx={{ mt: "10rem" }} gap={2} columns={[1, null, 3]}>
       <Box>
         <h3 sx={{ mb: "7rem", fontSize: "2.8rem" }}>
@@ -23,7 +25,7 @@ const Integrations = () => {
         Everyone is convenient with a tool, we respect your choice.
       </Box>
 
-      <Box className="integrations" >
+      <Box className="integrations">
         <Flex className="icons-holder">
           <div>
             <FigmaIcon />
@@ -82,7 +84,42 @@ const Integrations = () => {
         </Flex>
       </Box>
     </Grid>
-  )
-}
+
+    <div
+      sx={{
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Link
+        to="https://app.iconbox.space"
+        target="_blank"
+        sx={{
+          display: "flex",
+          padding: "0.5rem 1.6rem",
+          fontSize: "1.2rem",
+          fontWeight: 500,
+          background: "#fff",
+          color: "rgb(108 187 71)",
+          borderRadius: "12px",
+          textDecoration: "none",
+          height: "3rem",
+          marginTop: "120px",
+          boxShadow: "0 12px 47px -14px rgb(108 187 71)",
+          zIndex: 9,
+          textTransform: "uppercase",
+          letterSpacing: 1,
+          fontWeight: 300,
+          ":hover svg": {
+            transform: "translateX(0.7rem)",
+          },
+        }}
+      >
+        Start using app
+      </Link>
+    </div>
+  </>
+)
 
 export default Integrations
